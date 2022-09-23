0707394dc438362b2fc3d499219454999a244b40
Abinash Karmakar <codeAbinash@gmail.com>
2022-09-23 10:51:12 +0530

    Added service worker, now works offline

--- js/script.js
+++ js/script.js
@@ -56,12 +56,19 @@ async function registerSW() {
     if ('serviceWorker' in navigator) {
         try {
             // Change the service worker URL to see what happens when the SW doesn't exist
-            const registration = await navigator.serviceWorker.register("./js/sw.js");
+            const registration = await navigator.serviceWorker.register("./sw.js");
+            // console.log('Service worker registered')
         } catch (error) {
             showResult("Error while registering: " + error.message);
+            console.log(error)
         }
     } else {
         showResult("Service workers API not available");
     }
 };
 registerSW()
+
+
+function showResult(data) {
+    console.log(data)
+}
